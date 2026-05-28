a88997ac4837187d056e8ded43eed67d8819baf3
voleduy7 <voleduy7@gmail.com>
2026-05-28 20:58:35 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // File cấu hình tập trung cho toàn bộ hệ thống BAETON Badminton
 const CONFIG = {
     // Dán URL Web App Google Apps Script mới nhất của bạn vào đây
-    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxQ6264QtIwJ3QSam94--T2f7dHiOXPqW6qU6YK4BWyFFpJRgXPM0mnNcl6x-4v-rVvlw/exec"
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbznBU6nnoqmHUaIAmpZpH4j762Nl6VbmR7lfF8-KAf65gGBvrncfltrzCnjE6KhV8eU/exec"
 };
